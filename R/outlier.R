b7112ec7fe0ef4448b0acb7fcb4ea6857ab0f311
FARHAD SALIMI <FARHADSALIMI84@GMAIL.COM>
2021-12-29 13:00:59 +1100

    changed the outlier function to get a dataframe and gives out outlier status of all the numerical variables

--- R/outlier.R
+++ R/outlier.R
@@ -1,18 +1,31 @@
-#' Identifies outliers using a robust z-score
-#' @description This function takes one argument, the number of median absolute deviations outside which to assign a particular data point as an outlier.
-#'
-#' @param x the numeric vector of interest
-#' @param n the number of median absolute deviations from the median outside which to assign a datum as an outlier (default = 3)
-#'
-#' @return the outlier status (1, 0)
-#' @export
-#'
-#' @examples
-outlier <- function(x, n = 3) {
+outlier_detect <- function(x, n) {
   output <-
     !dplyr::between(x,
                     median(x, na.rm = TRUE) - n*mad(x, na.rm = TRUE),
                     median(x, na.rm = TRUE) + n*mad(x, na.rm = TRUE)
-                    )
+    )
   as.numeric(output)
 }
+
+#' Identifies outliers using a robust z-score
+#' @description This function takes a dataframe and one argument (i.e. the number of median absolute deviations outside which to assign a particular data point as an outlier).
+#'
+#' @param data the data frame for which outliers need to be found
+#' @param n the number of median absolute deviations from the median outside which to assign a datum as an outlier (default = 3)
+#'
+#' @return the dataframe with outlier status (1, 0) for all the numerical variables
+#' @export
+#'
+#' @examples
+#'
+
+
+outlier <- function(data, n = 3) {
+  data %>%
+    mutate(across(where(is.numeric),
+                  outlier_detect,
+                  n,
+                  .names = "outlier_{.col}"))%>%
+    dplyr::filter(if_any(contains("outlier"), ~ .x == 1))
+}
+
